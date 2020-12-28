44ff2b439486179ee706c004d023d22b3357e674
Stefano <s.vanin@outlook.it>
2020-12-28 14:42:26 +0100

    initialized standard analysis with hr-zones, modified architecture, utilities and configuration accordingly

--- CyPerAna/cyPerAna.py
+++ CyPerAna/cyPerAna.py
@@ -1,14 +1,15 @@
 import logging
 
 from .reader import FitReader
-from .utilities import get_configuration
+from .utilities import get_athlete_parameters, get_cardio_zones, get_cardio_zone
 
 
 class Instance:
     def __init__(self, athlete='default'):
         self.logger = logging.getLogger('CyPerAna.' + self.__class__.__name__)
         self.athlete = athlete
-        self.athlete_parameters = get_configuration(self.athlete)
+        self.athlete_parameters = get_athlete_parameters(self.athlete)
+        self.cardio_zones = get_cardio_zones(self.athlete_parameters["cardio-zones-model"])
         self.data = {}
         self.logger.info("Initialized instance of CyPerAna")
 
@@ -16,8 +17,25 @@ class Instance:
         ext = path.split(".")[-1]
         if ext == "fit":
             reader = FitReader(path)
-            self.data["#TODO"] = reader.data
+            self.data["#TODO"] = reader.data.set_index("timestamp")
             self.logger.info(path + " stored in self.data[#TODO]")
         else:
             self.logger.info("type f file not implemented yet")
+
+        self.standardRoutine("#TODO")
+
+    def standardRoutine(self, key):
+        self.logger.info("Starting standard analysis ruotine")
+        self.athleteSpecificAnalysis(key)
+
+    def athleteSpecificAnalysis(self, key):
+        self.logger.info("Excuting athlete-specific analysis")
+        self.data[key]["cardio_zone"] = self.data[key]["heart_rate"].apply(
+            lambda x: get_cardio_zone(x,
+                                      self.athlete_parameters["max-hr"],
+                                      self.cardio_zones))
+        # TODO: very bad computational performance here
+
+    def notAthleteSpecificAnalysis(self):
+        pass
 
--- CyPerAna/utilities.py
+++ CyPerAna/utilities.py
@@ -1,16 +1,70 @@
+from datetime import datetime as dt
 import os
-
+from pandas import to_datetime as td
 import yaml
 
 THIS_DIR = os.path.dirname(os.path.realpath(__file__))
 
 
-def get_configuration(athlete):
+def get_configuration():
     conf_path = THIS_DIR + '\\configuration.yml'
     with open(conf_path) as configuration:
         configuration = yaml.safe_load(configuration)
-    return configuration["athlete"][athlete]
+    return configuration
 
+
+def get_athlete_parameters(athlete):
+    athlete_parameters = get_configuration()["athlete"][athlete]
+    if not athlete_parameters["max-hr"]:
+        athlete_parameters["max-hr"] = calculate_max_hr(athlete_parameters["birth"])
+
+    return athlete_parameters
+
+
+def get_cardio_zones(model):
+    configuration = get_configuration()
+    return configuration["cardio-zones"][model]
+
+
+def get_cardio_zone(hr, hr_max, cardio_zones):
+    """
+    From https://www.garmin.com/en-US/blog/general/get-zone-train-using-heart-rate/
+    Zone 1: light intensity, walking pace;
+    Zone 2: warm-up, cool-down;
+    Zone 3: long effort;
+    Zone 4: tempo effort;
+    Zone 5: sprint effort;
+    """
+
+    pcg = hr / hr_max
+    zone = 0
+    for key in cardio_zones.keys():
+        if pcg > float(cardio_zones[key][0]):
+            zone += 1
+
+    if pcg > 1:
+        zone = "over-max"
+
+    return zone
 
 def do_nothing(*args):
     pass
+
+
+def calculate_max_hr(birth):
+    """
+    From Garmin blog
+    """
+    age = get_age(birth)
+    return 220 - age
+
+
+def get_age(birth):
+    now = dt.now()
+    birth_dt = td(birth)
+    age = now.year - birth_dt.year
+    if birth_dt.month >= now.month:
+        if birth_dt.month == now.month:
+            if birth_dt.day > now.day:
+                age -= 1
+    return age
