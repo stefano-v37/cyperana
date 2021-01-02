0fb007acb5e734c45ae87742a2e7d38fc612c66b
Stefano <s.vanin@outlook.it>
2021-01-02 23:05:41 +0100

    added max and avg values for hr, power, torque

--- CyPerAna/workout.py
+++ CyPerAna/workout.py
@@ -1,5 +1,5 @@
 import logging
-
+from collections import OrderedDict
 from pandas import DataFrame, MultiIndex
 
 from .utilities import calculate_energy, calculate_torque, get_cardio_zone, THIS_DIR, get_configuration, make_list_tuples_from_dict
@@ -10,7 +10,6 @@ class GenericWorkOut:
         self.id = None
         self.type = "generic"
         self.data = _data
-        self.data_avg = self.data_avg()
         self.date = _start.date()
         self.total_time = None
         self.wu_duration = None
@@ -19,32 +18,57 @@ class GenericWorkOut:
 
         self.total_energy = None
         self.fat_burned = None # kg
+        self.avg_hr = None
         self.avg_power = None
+        self.avg_torque = None
+        self.max_hr = None
         self.max_power = None
         self.max_torque = None
 
         self._log = None
         self.init_logger()
 
+    @property
     def data_avg(self):
         _mi = MultiIndex.from_tuples(make_list_tuples_from_dict(get_configuration(THIS_DIR + "\\reader\\configuration.yml")["columns"]))
         _data = self.data.copy()
         _data.columns = MultiIndex.from_tuples((x, "") for x in _data.columns)
         _data = DataFrame(columns=_mi).append(_data)
         avg_cols = [x for x in _data.columns if x[1] == "raw"]
-        _data[avg_cols] = _data[[(x[0], "") for x in avg_cols]]
+        _data[avg_cols] = _data[[(x[0], "") if (x[0], "") in _data.columns else (x[0], "raw") for x in avg_cols]]
         _data = _data.loc[:, [x for x in _data.columns if x not in [(x[0], "") for x in avg_cols]]]
 
         for key in _data.columns:
             if "s" in key[1]:
                 _data.loc[:, (key[0], key[1])] = _data[key[0]]["raw"].rolling(key[1]).mean()
 
-        return _data
+        return _data[_mi]
+
+    def add_avg_hr(self):
+        # TODO: distinguish in full, only workout, cool-down ecc.
+        self.avg_hr = self.data["heart-rate"].mean()
+
+    def add_avg_power(self):
+        # TODO: distinguish in full, only workout, cool-down ecc.
+        self.avg_power = self.data["power"].mean()
+
+    def add_avg_torque(self):
+        # TODO: distinguish in full, only workout, cool-down ecc.
+        self.avg_torque = self.data["torque"].mean()
 
     def add_energy(self):
         self.data["energy"] = calculate_energy(self.data["power"], self.data["duration"])
         self.total_energy = self.data["energy"].sum()
         self.fat_burned = self.total_energy / 37000000
+
+    def add_max_hr(self):
+        self.max_hr = {x[0] : x[1] for x in self.data_avg["heart-rate"].max().to_dict(into=OrderedDict).items() if x[0] != "raw"}
+
+    def add_max_power(self):
+        self.max_power = {x[0] : x[1] for x in self.data_avg["power"].max().to_dict(into=OrderedDict).items() if x[0] != "raw"}
+
+    def add_max_torque(self):
+        self.max_torque = {x[0] : x[1] for x in self.data_avg["torque"].max().to_dict(into=OrderedDict).items() if x[0] != "raw"}
 
     def add_torque(self):
         self.data["torque"] = calculate_torque(self.data["power"], self.data["cadence"])
@@ -75,6 +99,12 @@ class GenericWorkOut:
         self.set_total_time()
         self.add_torque()
         self.add_energy()
+        self.add_avg_hr()
+        self.add_avg_power()
+        self.add_avg_torque()
+        self.add_max_hr()
+        self.add_max_power()
+        self.add_max_torque()
 
 
 class TrainerWorkOut(GenericWorkOut):
